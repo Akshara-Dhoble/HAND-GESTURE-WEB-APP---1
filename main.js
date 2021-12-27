d172981639f5efbce17b9b3572387b9681defd68
Akshara-Dhoble <88190894+Akshara-Dhoble@users.noreply.github.com>
2021-12-27 14:40:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+var prediction = "";
+
 Webcam.set({
-    width:350,
+width:350,
 height:300,
 image_format : 'png' ,
 png_quality:90
@@ -17,16 +19,44 @@ function take_snapshot() {
 
 console.log('ml5 version', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/B5bIh-Rwo/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/sVDldMnvE/model.json', modelLoaded);
 
 function  modelLoaded() {
     console.log('Model Loaded');
 }
 
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "The prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
 function check() {
     img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
 }
-
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_hand_gesture").innerHTML = results[0].label;
+      prediction = results[0].label;
+      speak();
+      if(results[0].label == "victory")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#9996;";
+      }
+      if(results[0].label == "amazing")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "best")
+      {
+          document.getElementById("update_hand_gesture").innerHTML = "&#128077;";
+      }
+  
+    }
+  }
 
   
